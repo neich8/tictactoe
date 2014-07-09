45429b071169ce75ecb8fa8fe1c5678b1190bf05
Edward Price <ted.price@gmail.com>
2014-07-08 23:08:32 -0500

    fix computers

--- lib/computer.rb
+++ lib/computer.rb
@@ -3,54 +3,54 @@ require "byebug"
 module Computer
   extend self
 
-	def two_in_a_row(board, piece)
+  def two_in_a_row(board, piece)
     two_in_a_(:find_rows, board, piece)
-	end
+  end
 
-	def two_in_a_column(board, piece)
+  def two_in_a_column(board, piece)
     two_in_a_(:find_columns, board, piece)
-	end
+  end
 
-	def two_in_a_diagonal(board, piece)
+  def two_in_a_diagonal(board, piece)
     two_in_a_(:find_diagonals, board, piece)
-	end
+  end
 
-	def row_includes?(board, piece)
+  def row_includes?(board, piece)
     something_includes?(:find_rows, board, piece)
-	end
+  end
 
-	def column_includes?(board, piece)
+  def column_includes?(board, piece)
     something_includes?(:find_columns, board, piece)
-	end
+  end
 
-	def diagonal_includes?(board, piece)
+  def diagonal_includes?(board, piece)
     something_includes?(:find_diagonals, board, piece)
-	end
+  end
 
-	def two_way_loses(board,piece)
+  def two_way_loses(board,piece)
 
-	end
+  end
 
-	def find_piece(piece)
-		if piece == "X"
-			"O"
-		else
-			"X"
-		end
-	end
+  def find_piece(piece)
+    if piece == "X"
+      "O"
+    else
+      "X"
+    end
+  end
 
   private
 
   def two_in_a_(method, board, piece)
-		res = []
-		board.send(method).each do |set|
-			if set.count(piece) == 2 
-				unless set.include?(find_piece(piece))
+    res = []
+    board.send(method).each do |set|
+      if set.count(piece) == 2 
+        unless set.include?(find_piece(piece))
           res << set.select(&:available?)
-				end
-			end
-		end
-		res.flatten
+        end
+      end
+    end
+    res.flatten
   end
 
   def something_includes?(method, board, piece)
