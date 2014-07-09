63b39e7eea84252f8c211bcf11d626eacf6b1055
Edward Price <ted.price@gmail.com>
2014-07-08 22:36:09 -0500

    find_side_length => side_length

--- lib/board.rb
+++ lib/board.rb
@@ -1,77 +1,68 @@
 class Board
-attr_reader  :side_length, :corners, :diagonals, :game_board, :rows, :columns
-	def initialize
-		@corners = []
-		find_side_length
-	end
-
-  def game_board
-		@game_board ||= (1..9).to_a
+  attr_reader  :corners, :diagonals, :game_board, :rows, :columns
+  def initialize
+    @corners = []
   end
 
-	def middle
-		game_board[(game_board.length) / 2]
-	end
+  def game_board
+    @game_board ||= (1..9).to_a
+  end
 
-	def find_corners
-		corner_seperation = @side_length - 1
-		corners[0] = game_board.first
-		corners[1] = game_board[0 + corner_seperation]
-		corners[2] = game_board[(game_board.length - 1) - corner_seperation]
-		corners[3] = game_board.last
-	end
+  def middle
+    game_board[(game_board.length) / 2]
+  end
 
-	def find_rows
-		@rows = game_board.each_slice(@side_length).to_a
-	end
+  def find_corners
+    corner_seperation = side_length - 1
+    corners[0] = game_board.first
+    corners[1] = game_board[0 + corner_seperation]
+    corners[2] = game_board[(game_board.length - 1) - corner_seperation]
+    corners[3] = game_board.last
+  end
 
-	def find_columns
-		@columns = find_rows.transpose
-	end
+  def find_rows
+    @rows = game_board.each_slice(side_length).to_a
+  end
 
-	def find_diagonals
-		@diagonals = []
-		@diagonals << diag_down_left
-		@diagonals << diag_down_right
-	end
+  def find_columns
+    @columns = find_rows.transpose
+  end
 
-	def display
-		game_board.each_slice(3).to_a.each {|row|
-	 	p row}
-	end
+  def find_diagonals
+    @diagonals = []
+    @diagonals << diag_down_left
+    @diagonals << diag_down_right
+  end
+
+  def display
+    game_board.each_slice(3).to_a.each {|row|
+      p row}
+  end
 
 
-	private
+  private
 
-	def diag_down_right
-		down_left = []
-		diagonal_spot = @side_length - 1
-		find_rows.each do |row|
-			down_left << row[diagonal_spot]
-			diagonal_spot -= 1
-		end
-		down_left
-	end
+  def diag_down_right
+    down_left = []
+    diagonal_spot = side_length - 1
+    find_rows.each do |row|
+      down_left << row[diagonal_spot]
+      diagonal_spot -= 1
+    end
+    down_left
+  end
 
-	def diag_down_left
-		down_right = []
-		diagonal_spot = 0
-		find_rows.each do |row|
-	 		down_right << row[diagonal_spot]
-	 		diagonal_spot += 1
-		end
-		down_right
-	end
+  def diag_down_left
+    down_right = []
+    diagonal_spot = 0
+    find_rows.each do |row|
+      down_right << row[diagonal_spot]
+      diagonal_spot += 1
+    end
+    down_right
+  end
 
-	def find_side_length
-		area = game_board.length
-		guess = game_board.length 
-		while guess > 0
-			if guess * guess == area
-				@side_length = guess
-				break
-			end
-			guess -= 1
-		end
-	end
+  def side_length
+    @side_length ||= Math.sqrt game_board.length
+  end
 end
